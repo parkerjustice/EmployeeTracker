3bb16b65d134014207f42195d6a74a5e78b2431b
parkerjustice <you@example.com>
2022-03-13 15:33:24 -0400

    added sql

---
+++ seed.sql
@@ -0,0 +1,28 @@
+USE DBemployee;
+
+INSERT INTO department (name)
+VALUES 
+('Production'),
+('Home Office'),
+('Creative'),
+('Camera'),
+('Marketing'),
+('HR');
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+('Production Assistant', 40000, 10),
+('Production Manager', 120000, 3),
+('Creative Specialist', 55000, 5),
+('Ideas', 55000, 10),
+('HR Assistant', 75000, 3),
+('Personal Assistant', 40000, 5);
+
+INSERT INTO employee (first, last, role, ID)
+VALUES 
+('John', 'Smith', 1, 458),
+('Ronald', 'Young', 2, 276),
+('David', 'Miller', 3, 486),
+('Maria', 'Hall', 4, 126),
+('Linda', 'Martin', 5, 724),
+('Taylor', 'Wilson', 6, 157);
