5b2c89783c8f79358b89b41964fe9e40d248b084
parkerjustice <you@example.com>
2022-03-13 15:07:55 -0400

    addinitial js

---
+++ app.js
@@ -0,0 +1,77 @@
+const mysql = require('mysql');
+const consoleTable = require('console.table')
+const inquirer = require('inquirer');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'maggiemay233',
+    database: 'employees_db'
+})
+
+connection.connect(function(err){
+    if (err) throw err;
+    options();
+})
+
+function options() {
+    inquirer
+        .prompt({
+            name: 'action',
+            type: 'list',
+            message: 'Welcome to our employee database! What would you like to do?',
+            choices: [
+                    'View all employees',
+                    'View all departments',
+                    'View all roles',
+                    'Add an employee',
+                    'Add a department',
+                    'Add a role',
+                    'Update employee role',
+                    'Delete an employee',
+                    'EXIT'
+                    ]
+            }).then(function (answer) {
+                switch (answer.action) {
+                    case 'View all employees':
+                        viewEmployees();
+                        break;
+                    case 'View all departments':
+                        viewDepartments();
+                        break;
+                    case 'View all roles':
+                        viewRoles();
+                        break;
+                    case 'Add an employee':
+                        addEmployee();
+                        break;
+                    case 'Add a department':
+                        addDepartment();
+                        break;
+                    case 'Add a role':
+                        addRole();
+                        break;
+                    case 'Update employee role':
+                        updateRole();
+                        break;
+                    case 'Delete an employee':
+                        deleteEmployee();
+                        break;
+                    case 'EXIT': 
+                        exitApp();
+                        break;
+                    default:
+                        break;
+                }
+        })
+};
+function viewEmployees() {
+    var query = 'SELECT * FROM employee';
+    connection.query(query, function(err, res) {
+        if (err) throw err;
+        console.log(res.length + ' employees found!');
+        console.table('All Employees:', res); 
+        options();
+    })
+};
